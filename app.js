97a1dd1872d020371e95545a7b53edf51dd15b9e
Marcus Vinicius <vmvini@hotmail.com>
2016-08-03 22:51:32 -0300

    getting rid of unnecessary dependencies

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var path = require('path');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 //load database connection
 require('./api/database/db');
 
 var app = express();
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
 
 var api = require('./routes/api');
 app.use('/api', api);
